a24685f0f83b472c62315ed5e95cd264ffa956ed
Your Name <you@example.com>
2024-03-21 20:20:33 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -63,7 +63,7 @@ int sumaLista(List *L)
 {
    int suma = 0;
    int *dato = (int*)first(L);
-   while(*dato != NULL)
+   while(dato != NULL)
    {   
      suma += *dato;
      *dato = (int*)next(L);
