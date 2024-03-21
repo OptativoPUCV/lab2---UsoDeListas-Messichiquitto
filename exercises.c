3519f261afbe408c7798533e3a5271da5608e151
Your Name <you@example.com>
2024-03-21 21:18:42 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -119,7 +119,7 @@ La función verifica si la cadena de entrada tiene sus
 paraéntesis balanceados. Retorna 1 si están balanceados,
 0 en caso contrario.
 */
-
 int parentesisBalanceados(char *cadena) 
 {
+  return 0;
 }
